531fb8b527aa42783922924e922260f1ca12a0e6
Fabian Shallari <fabianshallari@gmail.com>
2018-04-13 23:59:24 +0200

    improve documentation for matrix utility methods

--- game/logic/matrix_utils.js
+++ game/logic/matrix_utils.js
@@ -1,4 +1,6 @@
-// finds repeated values in an array
+/*
+  Checks whether the passed array has consecutive repeated elemets of any value
+ */
 const arrayHasRepetitions = (array, { numberOfRepetitions = 4, filter = [] }) => {
 
   for (let index = 0, repeatedValue, repetitions = 1; index < array.length - 1; index++) {
@@ -28,19 +30,30 @@ const arrayHasRepetitions = (array, { numberOfRepetitions = 4, filter = [] }) =>
 };
 
 
-// creates a matrix prefilled with passed value param 
+/*
+  Creates a matrix with dimesions [rows] x [cols], prefilled with value  
+*/
 export const createMatrix = (rows, cols, value=0) => {
   return Array.from(Array(rows), () => Array(cols).fill(value));
 }
 
-// for each chunk (ie. column or row) of the matrix
-// checks whether the matrix has any repetitions
+/*
+  Checks whether each row (chunk) of the matrix has any repeated values
+ */
 export const matrixHasRepetitions = (matrix, { numberOfRepetitions = 4, filter = [] }) => {
   return matrix.some(chunk => arrayHasRepetitions(chunk, { numberOfRepetitions , filter }));
 }
 
-// creates a new matrix which contains all the diagonals of the original matrix as rows
-// traversal is done top-left => bottom-right
+/*
+  Creates a copy of the original matrix which contains all the diagonals
+  of the original
+
+  Example:
+  [ [ 1 , 2 ],        [ [ 1 ]
+    [ 3 , 4 ],    =>    [ 3 , 2 ] 
+    [ 5 , 6 ] ]         [ 5 , 4 ]
+                        [ 6 ] ]
+*/
 export const matrixDiagonals = matrix => {
 
   const rows = matrix.length || 0;
@@ -69,14 +82,27 @@ export const matrixDiagonals = matrix => {
   return diagonals;
 }
 
-// returns a copy which is a rotated version of the original
-// rotation is 90 degrees clockwise
+/* 
+  Returns a rotated copy of the original matrix
+  Rotation is 90 degrees clockwise
+
+  Example: 
+  [ [ 1 , 2 ],        [ [ 5 , 3 , 1 ]
+    [ 3 , 4 ],    =>    [ 6 , 4 , 2 ] ]
+    [ 5 , 6 ] ]  
+*/
 export const rotateMatrix = matrix => {
   return transposeMatrix(matrix).map(row => row.reverse());
 }
 
-// returns a copy which is a transposed matrix of the original,
-// i.e turns rows to columns and columns to rows
+/*
+  Return a transposed copy of the original matrix
+
+  Example: 
+  [ [ 1 , 2 ],        [ [ 1 , 3 , 5 ]
+    [ 3 , 4 ],    =>    [ 2 , 4 , 6 ] ]
+    [ 5 , 6 ] ]         
+*/
 export const transposeMatrix = matrix => {
   const rows = matrix.length || 0;
   const cols = matrix[0] ? matrix[0].length : 0;
